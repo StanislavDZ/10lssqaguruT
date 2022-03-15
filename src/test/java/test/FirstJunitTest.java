6205b0e9aca0b532f392fc7fe86bdf4d801b5b21
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-03-15 21:27:48 +0300

    work resolution 700x1080

--- src/test/java/test/FirstJunitTest.java
+++ src/test/java/test/FirstJunitTest.java
@@ -17,40 +17,50 @@ public class FirstJunitTest extends TestBase {
     @Test
     void FormRun() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        open("/automation-practice-form");
-        $("#firstName").setValue("Stanislav");
-        $("#lastName").setValue("Zaychenko");
-        $("#userEmail").setValue("Ashestodust@yandex.ru");
-        $(byText("Male")).click();
-        $("#userNumber").setValue("9147851961");
-        $("#dateOfBirthInput").sendKeys(Keys.CONTROL, "a");
-        $("#dateOfBirthInput").sendKeys("13 apr 1987", Keys.ENTER);
-        $("#subjectsInput").setValue("Comp").pressEnter();
-        $("#subjectsInput").setValue("Ar").pressEnter();
-        $(byText("Sports")).scrollTo().click();
-        $(byText("Reading")).click();
-        $(byText("Music")).click();
-        //$("#uploadPicture").uploadFile(new File("src/test/resources/pic.png"));
-        $("#currentAddress").setValue("Taxes, BestCh.St 17");
-        $("#submit").scrollTo();
-        $("#state").click();
-        $(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $(byText("Submit")).click();
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Stanislav Zaychenko"));
-        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("Ashestodust@yandex.ru"));
-        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
-        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9147851961"));
-        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("13 April,1987"));
-        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Computer Science, Arts"));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Reading, Music"));
-        //$(".table-responsive").$(byText("Picture")).parent().shouldHave(text("pic.png"));
-        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Taxes, BestCh.St 17"));
-        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
-        $(byText("Close")).scrollTo().click();
+        step("Открываем главную страницу", () -> {
+            open("/automation-practice-form");
+        });
+        step("Заполняем форму регистрационными данными:", () -> {
+            $("#firstName").setValue("Stanislav");
+            $("#lastName").setValue("Zaychenko");
+            $("#userEmail").setValue("Ashestodust@yandex.ru");
+            $(byText("Male")).click();
+            $("#userNumber").setValue("9147851961");
+            $("#dateOfBirthInput").sendKeys(Keys.CONTROL, "a");
+            $("#dateOfBirthInput").sendKeys("13 apr 1987", Keys.ENTER);
+            $("#subjectsInput").setValue("Comp").pressEnter();
+            $("#subjectsInput").setValue("Ar").pressEnter();
+            $(byText("Sports")).scrollTo().click();
+            $(byText("Reading")).click();
+            $(byText("Music")).click();
+            //$("#uploadPicture").uploadFile(new File("src/test/resources/pic.png"));
+            $("#currentAddress").setValue("Taxes, BestCh.St 17");
+            $("#submit").scrollTo();
+            $("#state").click();
+            $(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+        });
+        step("Подтверждаем заполнение формы", () -> {
+            $(byText("Submit")).click();
+        });
+        step("Проверяем заполнение формы", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Stanislav Zaychenko"));
+            $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("Ashestodust@yandex.ru"));
+            $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+            $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9147851961"));
+            $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("13 April,1987"));
+            $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Computer Science, Arts"));
+            $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Reading, Music"));
+              //$(".table-responsive").$(byText("Picture")).parent().shouldHave(text("pic.png"));
+            $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Taxes, BestCh.St 17"));
+            $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+        });
 
+        step("Закрываем форму с данными", () -> {
+            $(byText("Close")).scrollTo().click();
+        });
 
     }
 }
