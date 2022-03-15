ed9eb8022007455e3a774bdccfe1be3ab167f02d
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-03-15 19:13:49 +0300

    Update FirstJunitTest.java

--- src/test/java/test/FirstJunitTest.java
+++ src/test/java/test/FirstJunitTest.java
@@ -18,6 +18,10 @@ public class FirstJunitTest {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "700x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC",true);
+        capabilities.setCapability("enableVideo",true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @Test
